120ae6aae5af9c84627532ea42f806aed86661fe
Joel <joeldome17@gmail.com>
2021-08-05 19:07:06 -0500

    Digital clock and Audible alarm

--- js/ejercicios-practicos-dom/76.index.js
+++ js/ejercicios-practicos-dom/76.index.js
@@ -1,8 +1,12 @@
 import hamburgerButton from './hamburger_button.js';
+import {clockFunction, alarmaFunction} from './reloj_alarma.js';
+
 
 //Añadiendo eventos con 'DOMContentLoaded'
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e)=>{
     hamburgerButton("#hamburger-button", "#menu", "#menu ul li a");
-})
+    clockFunction("#btnIniciar", "#btnDetener");
+    alarmaFunction("#alarmaIniciar", "#alarmaDetener");
+});
