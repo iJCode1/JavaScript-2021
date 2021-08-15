caf3349fbb7bc42891b95a5c1df7b64bbcaefc02
Joel <joeldome17@gmail.com>
2021-08-15 03:28:37 -0500

    Search filter finished

--- js/ejercicios-practicos-dom/76.index.js
+++ js/ejercicios-practicos-dom/76.index.js
@@ -9,7 +9,8 @@ import tester from "./responsive_tester.js";
 import deteccionDispositivos from "./deteccion_dispositivos.js";
 import deteccionRed from "./deteccion-red.js";
 import deteccionWebCam from "./deteccion_webcam.js";
-import detectarGeolocalizacion from './geolocalizacion.js';
+import detectarGeolocalizacion from "./geolocalizacion.js";
+import filter from "./filtro_busqueda.js";
 
 //Añadiendo eventos con 'DOMContentLoaded'
 const d = document;
@@ -33,8 +34,12 @@ d.addEventListener("DOMContentLoaded", (e) => {
   tester("#form");
   deteccionDispositivos("#deteccion-dispositivos");
   deteccionRed();
-  deteccionWebCam("#section8", {audio: true, video: {width: 1280, height: 720}});
+  deteccionWebCam("#section8", {
+    audio: true,
+    video: { width: 1280, height: 720 },
+  });
   detectarGeolocalizacion("#section9");
+  filter("#search", ".clearSearch", ".cards__filter");
 });
 
 modeDark("#btnModeDark");
--- js/ejercicios-practicos-dom/mode-dark.js
+++ js/ejercicios-practicos-dom/mode-dark.js
@@ -1,82 +1,124 @@
 const d = document;
 
-export default function modeDark(boton){
-    const $btnModeDark = d.querySelector(boton);
+export default function modeDark(boton) {
+  const $btnModeDark = d.querySelector(boton);
 
-    d.addEventListener("click", (e)=>{
-        if(e.target.matches(boton)){
-            document.body.classList.toggle("modeDark");
-            $btnModeDark.classList.toggle("btnModeLight");
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(boton)) {
+      document.body.classList.toggle("modeDark");
+      $btnModeDark.classList.toggle("btnModeLight");
 
-            document.body.querySelector("h1").classList.toggle("background-dark");
-            
-            let $sections = document.body.querySelectorAll("section");
-            $sections.forEach((e)=>{
-                e.classList.toggle("colorDark");
-            });
+      document.body.querySelector("h1").classList.toggle("background-dark");
 
-            document.body.querySelector("#keyboard-wrapper").classList.toggle("keyboard-wrapper-dark");
+      let $sections = document.body.querySelectorAll("section");
+      $sections.forEach((e) => {
+        e.classList.toggle("colorDark");
+      });
 
-            
-            /*$btnModeDark.classList.forEach((clase)=>{
+      document.body
+        .querySelector("#keyboard-wrapper")
+        .classList.toggle("keyboard-wrapper-dark");
+
+      /*$btnModeDark.classList.forEach((clase)=>{
                 if(clase === "btnModeLight"){
                     // localStorage.setItem("modo", "modeDark");
                 }else{
                     localStorage.removeItem("modo");
                 }
             })*/
-            localStorage.setItem("modo", d.body.classList);
-            localStorage.setItem("modoH1", d.body.querySelector("h1").classList);
+      localStorage.setItem("modo", d.body.classList);
+      localStorage.setItem("modoH1", d.body.querySelector("h1").classList);
 
-            localStorage.setItem("modoSection1", d.body.querySelector("#section1").classList);
-            localStorage.setItem("modoSection2", d.body.querySelector("#section2").classList);
-            localStorage.setItem("modoSection3", d.body.querySelector("#section3").classList);
-            localStorage.setItem("modoSection4", d.body.querySelector("#section4").classList);
-            localStorage.setItem("modoSection5", d.body.querySelector("#section5").classList);
-            localStorage.setItem("modoSection6", d.body.querySelector("#section6").classList);
-            localStorage.setItem("modoSection7", d.body.querySelector("#section7").classList);
-            localStorage.setItem("modoSection8", d.body.querySelector("#section8").classList);
-            localStorage.setItem("modoSection9", d.body.querySelector("#section9").classList);
+      localStorage.setItem(
+        "modoSection1",
+        d.body.querySelector("#section1").classList
+      );
+      localStorage.setItem(
+        "modoSection2",
+        d.body.querySelector("#section2").classList
+      );
+      localStorage.setItem(
+        "modoSection3",
+        d.body.querySelector("#section3").classList
+      );
+      localStorage.setItem(
+        "modoSection4",
+        d.body.querySelector("#section4").classList
+      );
+      localStorage.setItem(
+        "modoSection5",
+        d.body.querySelector("#section5").classList
+      );
+      localStorage.setItem(
+        "modoSection6",
+        d.body.querySelector("#section6").classList
+      );
+      localStorage.setItem(
+        "modoSection7",
+        d.body.querySelector("#section7").classList
+      );
+      localStorage.setItem(
+        "modoSection8",
+        d.body.querySelector("#section8").classList
+      );
+      localStorage.setItem(
+        "modoSection9",
+        d.body.querySelector("#section9").classList
+      );
+      localStorage.setItem(
+        "modoSection10",
+        d.body.querySelector("#section10").classList
+      );
 
-            localStorage.setItem("modoSection2Background", d.body.querySelector("#keyboard-wrapper").classList);
+      localStorage.setItem(
+        "modoSection2Background",
+        d.body.querySelector("#keyboard-wrapper").classList
+      );
 
-            localStorage.setItem("btnModeImage", d.body.querySelector("#btnModeDark").classList);
-            // console.log($btnModeDark.classList);
-        }
-    });
+      localStorage.setItem(
+        "btnModeImage",
+        d.body.querySelector("#btnModeDark").classList
+      );
+      // console.log($btnModeDark.classList);
+    }
+  });
 
-    d.addEventListener("DOMContentLoaded", e =>{
-        if(localStorage.length !== 0){
-            const modo = localStorage.getItem("modo");
-            const modoH1 = localStorage.getItem("modoH1");
-            const modoSection1 = localStorage.getItem("modoSection1");
-            const modoSection2 = localStorage.getItem("modoSection2");
-            const modoSection3 = localStorage.getItem("modoSection3");
-            const modoSection4 = localStorage.getItem("modoSection4");
-            const modoSection5 = localStorage.getItem("modoSection5");
-            const modoSection6 = localStorage.getItem("modoSection6");
-            const modoSection7 = localStorage.getItem("modoSection7");
-            const modoSection8 = localStorage.getItem("modoSection8");
-            const modoSection9 = localStorage.getItem("modoSection9");
-            const modoSection2Background = localStorage.getItem("modoSection2Background");
-            const modoBtnMode = localStorage.getItem("btnModeImage");
-    
-            d.body.classList = modo;
-            d.querySelector("h1").classList = modoH1;
-            
-            d.querySelector("#section1").classList = modoSection1;
-            d.querySelector("#section2").classList = modoSection2;
-            d.querySelector("#section3").classList = modoSection3;
-            d.querySelector("#section4").classList = modoSection4;
-            d.querySelector("#section5").classList = modoSection5;
-            d.querySelector("#section6").classList = modoSection6;
-            d.querySelector("#section7").classList = modoSection7;
-            d.querySelector("#section8").classList = modoSection8;
-            d.querySelector("#section9").classList = modoSection9;
-    
-            d.querySelector("#keyboard-wrapper").classList = modoSection2Background;
-            
-            d.querySelector("#btnModeDark").classList = modoBtnMode;
-        }
-    });
+  d.addEventListener("DOMContentLoaded", (e) => {
+    if (localStorage.length !== 0) {
+      const modo = localStorage.getItem("modo");
+      const modoH1 = localStorage.getItem("modoH1");
+      const modoSection1 = localStorage.getItem("modoSection1");
+      const modoSection2 = localStorage.getItem("modoSection2");
+      const modoSection3 = localStorage.getItem("modoSection3");
+      const modoSection4 = localStorage.getItem("modoSection4");
+      const modoSection5 = localStorage.getItem("modoSection5");
+      const modoSection6 = localStorage.getItem("modoSection6");
+      const modoSection7 = localStorage.getItem("modoSection7");
+      const modoSection8 = localStorage.getItem("modoSection8");
+      const modoSection9 = localStorage.getItem("modoSection9");
+      const modoSection10 = localStorage.getItem("modoSection10");
+      const modoSection2Background = localStorage.getItem(
+        "modoSection2Background"
+      );
+      const modoBtnMode = localStorage.getItem("btnModeImage");
+
+      d.body.classList = modo;
+      d.querySelector("h1").classList = modoH1;
+
+      d.querySelector("#section1").classList = modoSection1;
+      d.querySelector("#section2").classList = modoSection2;
+      d.querySelector("#section3").classList = modoSection3;
+      d.querySelector("#section4").classList = modoSection4;
+      d.querySelector("#section5").classList = modoSection5;
+      d.querySelector("#section6").classList = modoSection6;
+      d.querySelector("#section7").classList = modoSection7;
+      d.querySelector("#section8").classList = modoSection8;
+      d.querySelector("#section9").classList = modoSection9;
+      d.querySelector("#section10").classList = modoSection10;
+
+      d.querySelector("#keyboard-wrapper").classList = modoSection2Background;
+
+      d.querySelector("#btnModeDark").classList = modoBtnMode;
+    }
+  });
 }
